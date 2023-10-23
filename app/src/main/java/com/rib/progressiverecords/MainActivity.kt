a3866d0355791822af3dced2bea29330ead8ca98
Nanuk <nanuk.rj@gmail.com>
2023-10-23 17:51:23 +0200

    App version 1.0.1 released

--- app/src/main/java/com/rib/progressiverecords/MainActivity.kt
+++ app/src/main/java/com/rib/progressiverecords/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
--- app/src/main/java/com/rib/progressiverecords/ui/theme/ComposableTemplates.kt
+++ app/src/main/java/com/rib/progressiverecords/ui/theme/ComposableTemplates.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
